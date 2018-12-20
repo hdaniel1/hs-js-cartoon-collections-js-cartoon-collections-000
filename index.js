fef953e2b25ec9eda6de0a8d742bb81d10e7da96
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-20 22:35:33 +0000

    Done.

--- index.js
+++ index.js
@@ -25,13 +25,7 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
   for (let i = 0; i < foods.length; i++) {
-    if (foods[i] === 'cheddar') {
-      return foods[i]
-    }
-    else if (foods[i] === 'gouda') {
-      return foods[i]
-    }
-    else if (foods[i] === 'camembert') {
+    if (foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert') {
       return foods[i]
     }
   }
