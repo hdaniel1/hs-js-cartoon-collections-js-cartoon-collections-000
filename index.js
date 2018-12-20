c61081eb2e31a0f4561bf30997b2ddc4fd34f613
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-20 22:24:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function dwarfRollCall(dwarves) {
+  var stored = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    stored.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return stored.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i] = `${planeteerCalls[i].toUpperCase()}!`
+  }
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true
+    }
+    return false
+
+  }
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === 'cheddar') {
+      return foods[i]
+    }
+    else if (foods[i] === 'gouda') {
+      return foods[i]
+    }
+    else if (foods[i] === 'camembert') {
+      return foods[i]
+    }
+  }
+  return 'no cheese!'
 }
